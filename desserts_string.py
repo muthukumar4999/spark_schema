d7f5a7fc4d632abded8683f679c0b5bf982aef5a
Muthu Kumar <muthukumar4999@gmail.com>
2022-06-13 19:44:45 +0530

    Create desserts_string.py

---
+++ desserts_string.py
@@ -0,0 +1,19 @@
+desserts = """[{
+"type": "donut",
+"name": "Cake",
+"ppu": 0.55,
+"batters":
+    {
+        "batter":
+            [
+                { "id": "1001", "type": "Regular" },
+                { "id": "1002", "type": "Chocolate" }
+            ]
+    },
+"topping":
+    [
+        { "id": "5001", "type": "None" },
+        { "id": "5002", "type": "Glazed" }
+    ]
+}
+]"""
